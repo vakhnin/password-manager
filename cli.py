ac3aa046a2453490a6e639ecf3fea83b7a9ce6f2
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-11-14 14:49:45 +0300

    Переделка структуры на обработку подкоманд

--- cli.py
+++ cli.py
@@ -1,57 +1,26 @@
 # cli.py
 import click
 import os
-import encryption
 
 
-def get_passwords(username, password):
-    """
-    This function extracts passwords table from database
-    """
-    os.chdir("databases" + os.sep + username)
-    # Function decrypts database
-    cipher = encryption.AESCipher(password)
-    with open(username + ".database", "r") as f:
-        encrypted = f.read()
-        text = cipher.decrypt(encrypted)
-    if not text:
-        os.chdir("..")
-        return None
-
-    # Then from decrypted text it extracts line with login and password
-    text = text.splitlines()
-    passwords_table = {}
-    for line in text:
-        line = line.strip().split(':')
-        passwords_table[line[0]] = line[1]
-    os.chdir('..')
-    os.chdir('..')
-    return passwords_table
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    pass
 
 
-@click.command()
-@click.argument('command')
+@cli.command()
 @click.option('-u', '--user')
 @click.option("--user", prompt="Username", help="Provide your username",
               default=lambda: os.environ.get('USERNAME'))
 @click.option('-p', '--password')
 @click.option('--password', prompt=True, hide_input=True)
-def main(command, user, password):
+def get(user, password):
     """
-    Функция выбора команды
+    get logins
     """
-    if command == 'show':  # Отображение всех сохраненных логинов
-        passwords_table = get_passwords(user, password)
-        if not passwords_table:
-            print("!!!Something went wrong. Database is corrupted or not exists!!!")
-        for login in passwords_table.keys():
-            print(login)
-    elif command == 'get':
-        print('get')
-    else:
-        print('No such command')
-        print('Try \'cli.py --help\' for help.')
+    click.echo('Command: get')
 
 
 if __name__ == "__main__":
-    main()
+    cli()
