e1e8ac0bfed97f35d68255aa1254c35bcf450711
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-12-05 16:09:11 +0300

    Настройка ротации логов раз в день

--- log_manager/models.py
+++ log_manager/models.py
@@ -2,6 +2,18 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
+
+def change_filename(filename):
+    filename_parts = filename.rsplit('.', maxsplit=2)
+    if filename_parts[-1] == 'log':
+        return filename
+    elif filename_parts[-2] == 'log':
+        return filename_parts[0] + '.' + filename_parts[2] + '.log'
+    else:
+        print('Внутренняя ошибка: базовый файл логирования не заканчивается на ".log"')
+        exit(-1)
+
+
 LOG = logging.getLogger('cli')
 
 FORMATTER = \
@@ -17,6 +29,9 @@ ROTATION_LOGGING_HANDLER = \
     TimedRotatingFileHandler(
         LOGS_PATH, when='D', interval=1, backupCount=5, encoding='utf-8')
 ROTATION_LOGGING_HANDLER.setFormatter(FORMATTER)
+ROTATION_LOGGING_HANDLER.setLevel(logging.DEBUG)
+ROTATION_LOGGING_HANDLER.namer = change_filename
+
 LOG.addHandler(ROTATION_LOGGING_HANDLER)
 
 LOG.setLevel(logging.DEBUG)
