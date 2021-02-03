3c07d4e165be6f77ffb0e8157bbb32a530cc2de2
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-02-03 15:44:04 +0300

    Почишен settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,3 @@
 import pathlib
 
 FILE_DB = pathlib.Path(__file__).parent / 'database.sqlite'
-
-LOGS_PATH = pathlib.Path.cwd() / 'logs' / 'common.log'
-
-TIME_SESSION_CLOSE = 15 * 60  # дефолтное время в секундах, отведенное на длительность сессии
-
-if not LOGS_PATH.parent.exists():
-    LOGS_PATH.parent.mkdir(parents=True)
-    LOGS_PATH.touch()
