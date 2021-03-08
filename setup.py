99190f2c397d0dfc4227e599b3cf73dbd13a18d8
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2021-03-08 16:27:15 +0300

    Change setup

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@
 # Создание установщика для Windows
 # $ python setup.py bdist --format=msi
 # Установщик в папке dist
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='pwdone',
     version='1.2',
     py_modules=['cli'],
     test_suite='tests',
+    packages=find_packages(),
     install_requires=[
         'click',
         'sqlalchemy',
